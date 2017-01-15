a30062def3ca1d506478fd46153065e0debde364
djudd <davidtjudd@gmail.com>
2017-01-15 14:47:54 -0500

    playing around

--- home/views.movies.js
+++ home/views.movies.js
@@ -31,7 +31,7 @@ define(['./spotlight', 'imageLoader', 'focusManager', 'cardBuilder', './../skini
         var options = {
 
             IncludeItemTypes: "Movie",
-            Limit: 12,
+            Limit: 112,
             ParentId: parentId,
             EnableImageTypes: "Primary,Backdrop,Thumb"
         };
--- skininfo.js
+++ skininfo.js
@@ -2,7 +2,7 @@ define([], function () {
     'use strict';
 
     return {
-        id: 'defaultskin',
-        name: 'Default Skin'
+        id: 'davidskin',
+        name: 'David\'s Skin'
     };
 });
