e04069c44f9eb977c5ef1c722c1c68cfdb2b896a
djudd <davidtjudd@gmail.com>
2017-01-15 15:23:17 -0500

    playing with sort

--- movies/movies.js
+++ movies/movies.js
@@ -236,7 +236,7 @@ define(['loading', 'alphaPicker', './../components/horizontallist', './../compon
                         ParentId: pageParams.parentid,
                         IncludeItemTypes: "Movie",
                         Recursive: true,
-                        SortBy: "SortName",
+                        SortBy: "DateCreated,SortName",
                         Fields: "SortName"
                     });
                 },
